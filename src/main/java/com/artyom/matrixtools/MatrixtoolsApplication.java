957c3cf5c2aa5480d91e784f488c981c97dd9e6e
Artyom Ivanov <artem_appstore@mail.ru>
2019-07-24 19:30:21 +0300

    Initial commit: project structure and frontend in progress

---
+++ src/main/java/com/artyom/matrixtools/MatrixtoolsApplication.java
@@ -0,0 +1,13 @@
+package com.artyom.matrixtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrixtoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artyom/matrixtools/MatrixtoolsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.artyom.matrixtools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MatrixtoolsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
